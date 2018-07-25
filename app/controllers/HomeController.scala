db13ec7196be651d845d173f184baa57bbdfa57e
robin <robin.van.rijn@ing.nl>
2018-07-25 14:17:09 +0200

    Added routes & placeholder functions for /recipe

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,7 +1,6 @@
 package controllers
 
 import javax.inject.Inject
-
 import play.api.mvc._
 
 /**
@@ -10,10 +9,29 @@ import play.api.mvc._
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
   def index = Action { implicit request =>
-    Ok(views.html.index())
+    Ok("Hello")
   }
 
+  /*  Placeholder functions */
+
   def test = Action { implicit request =>
-    Ok(views.html.test())
+    Ok("Can you hear me?")
   }
+
+  def getRecipe = Action { implicit request =>
+    Ok("This is the GET")
+  }
+
+  def postRecipe = Action { implicit request =>
+    Ok("This is the POST")
+  }
+
+  def deleteRecipe = Action { implicit request =>
+    Ok("This is the DELETE")
+  }
+
+  def putRecipe = Action { implicit request =>
+    Ok("This is the PUT")
+  }
+
 }
