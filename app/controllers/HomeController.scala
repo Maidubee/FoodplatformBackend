5a5c76754aea753b668114064b06b252a6853cc4
robin <robin.van.rijn@ing.nl>
2018-07-28 12:41:09 +0200

    Implemented GET to get a single recipe

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -5,25 +5,29 @@ import awscala.dynamodbv2._
 import javax.inject.Inject
 import play.api.mvc._
 
+import scala.util.{Failure, Success, Try}
+
 /**
   * A very small controller that renders a home page.
   */
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
+  implicit val dynamoDB: DynamoDB = DynamoDB.at(Region.Ireland)
   def index = Action { implicit request =>
     Ok("Hello, this is the index page")
   }
 
-  /*  Placeholder functions */
-
-  def getRecipe = Action { implicit request =>
-    implicit val dynamoDB = DynamoDB.at(Region.Ireland)
-    val table: Table = dynamoDB.table("recipes").get
-    // The .getS gets the String value, there is also getM to get a map, getL to get a list, etc.
-    table.getItem(1).get.attributes.foreach(item => println(item.name + " " + item.value.getS))
+  def getRecipe(id: Int) = Action { implicit request =>
+    id match{
+      case -1 => getAllRecipes
+      case _ => getSingleRecipe(id)
+    }
 
     Ok("This is the GET")
   }
+
+
+  /*  Placeholder functions */
 
   def postRecipe = Action { implicit request =>
     Ok("This is the POST")
@@ -35,6 +39,24 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
 
   def putRecipe = Action { implicit request =>
     Ok("This is the PUT")
+  }
+
+  /* Helper functions (to be moved later) */
+  def getTable(name: String) = {
+    dynamoDB.table(name).get
+  }
+
+  // The .getS gets the String value, there is also getM to get a map, getL to get a list, etc.
+  def getSingleRecipe(id: Int) = {
+    val table = getTable("recipes")
+    Try(table.getItem(id).get) match {
+      case Success(recipe) => recipe.attributes.foreach(item => println(item.name + " " + item.value.getS))
+      case Failure(_) => println(s"No recipe found with id $id.")
+    }
+  }
+
+  def getAllRecipes: Unit = {
+    println("Placeholder to get ALL recipes")
   }
 
 }
